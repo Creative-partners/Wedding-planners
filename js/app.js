965139eddc372119f388219574ee1254bb6e8b12
AyaAbuRashed <ayanaser402@gmail.com>
2020-09-27 12:59:29 +0300

    branch 1

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ function Branch(setting, location, image, size, isDjincluded, catring) {
 
 Branch.prototype.render = function () {
   for (let i = 0; i < size.length; i++) {
-    new Branch('indoor', 'adonees', , size[i], 'with DJ', 'with catring service');
+    new Branch('indoor', 'adonees',  size[i], 'with DJ', 'with catring service');
     new Branch('outdoor', 'Banafsaj', size[i], 'without DJ', 'with catring service');
     new Branch('outdoor', 'Country Club', size[i], 'without DJ', 'without catring service');
     new Branch('outdoor', 'dunes club', size[i], 'without DJ', 'without catring service');
@@ -48,4 +48,55 @@ Branch.prototype.render = function () {
   }
 
 };
+
+function onlyOne(checkbox) {
+    var checkboxes = document.getElementsByName('check')
+  
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+}
+
+form1.addEventListener('submit' , function (event){
+    event.preventDefault();
+ 
+     var bName = event.target.bName.value;
+     var lName = event.target.lName.value;
+     var gName = event.target.gName.value;
+     var glName = event.target.glName.value;
+     var wDate = event.target.date.value; 
+     var size = [document.getElementById('size0'),document.getElementById('size1'),document.getElementById('size2')];
+     var choosenSize ;
+       if(size[0].checked == true )
+       {choosenSize= event.target.size0.value; }
+       else if(size[1].checked == true ){
+       choosenSize= event.target.size1.value;
+       }
+       else{
+        choosenSize= event.target.size2.value;
+       }
+     var djIndex = [document.getElementById("dj1"),document.getElementById("dj2")];
+
+     if (djIndex[0].checked){
+         var dj = event.target.dj1.value;
+     }
+     
+     if (djIndex[1].checked){
+        var dj = event.target.dj2.value;
+    }
+    
+    var caIndex = [document.getElementById("ca1"),document.getElementById("ca2")];
+
+    if (caIndex[0].checked){
+        var catrering = event.target.ca1.value;
+    }
+    
+    if (caIndex[1].checked){
+       var catrering = event.target.ca2.value;
+   }
+   
+   console.log(bName,lName,gName,glName,wDate,choosenSize,dj,catrering);
+ 
+
+});
 
