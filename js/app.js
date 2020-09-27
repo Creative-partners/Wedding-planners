03e7d7cf5ccc65921077e3180b8787c34c371b31
Sally Tareq <sally.ammous@gmail.com>
2020-09-27 15:25:57 +0300

    added filter

--- js/app.js
+++ js/app.js
@@ -3,101 +3,200 @@
 var size = ['small', 'medium', 'large'];
 let branches = [];
 
-
-
-function Branch(setting, name, location, image, size, isDjincluded, catring) {
+// object to store locations
+function Branch(setting, name, location, image, size, isDjIncluded, catering, price) {
 
   this.setting = setting;
   this.name = name;
+  this.id = name.split(' ').slice(0, -1).toString();
   this.location = location;
   this.image = image;
   this.setting = setting;
   this.size = size;
-  this.isDjincluded = isDjincluded;
-  this.catring = catring;
+  this.isDjIncluded = isDjIncluded;
+  this.catering = catering;
+  this.price = price;
   branches.push(this);
 
 }
 
+//tests
+new Branch('indoor', 'Adonees Hall', 'Az zarqa/Army st.', 'img/adonees.png', 'small', 'yes', 'yes', '30');
+new Branch('indoor', 'Banafsaj Hall', 'Tlaa Al Ali - Amman', 'img/anafsaj.jpg', 'medium', 'no', 'yes', '30');
+new Branch('outdoor', 'Country Club', 'Action Target, Queen Alia Airport Road', 'img/Country Club.jpg', 'small', 'no', 'no', '45');
+new Branch('outdoor', 'Dunes Club', 'Close to Gamadan, Queen Alia Airport Road', 'img/dunes club.jpg', 'small', 'no', 'no', '50');
+new Branch('indoor', 'Fairmont Hotel', ' 5th Circle, Amman', 'img/fairmont.jpg', 'medium', 'yes', 'yes', '60');
+new Branch('outdoor', 'Four Seasons', ' Kindi St, Amman', 'img/fourseasons.jpg', 'small', 'no', 'no', '55');
+new Branch('indoor', 'Numan Hall', 'Wasfi At-Tall St., Amman', 'img/Numan.jpg', 'large', 'no', 'yes', '40');
+new Branch('indoor', 'Rotana Hotel', 'Black Iris Street, Amman', 'img/rotana.jpg', 'medium', 'yes', 'no', '60');
+new Branch('outdoor', 'W Amman', 'Rafiq Al Hariri Ave, Amman', 'img/W amman.jpg', 'medium', 'yes', 'yes', '50');
+new Branch('outdoor', 'white Halls', 'Queen Alia Airport Road', 'img/white halls.jpg', 'large', 'yes', 'no', '55');
 
-Branch.prototype.render = function () {
-  for (let i = 0; i < size.length; i++) {
-    new Branch('indoor', 'adonees','Az zarqa/Army st.', 'images/adonees.png' , size[i], 'with DJ', 'with catring service');
-    new Branch('outdoor', 'Banafsaj', 'Tlaa Al Ali - Amman' ,'images/anafsaj.jpg' , size[i], 'without DJ', 'with catring service');
-    new Branch('outdoor', 'Country Club','Action Target, Queen Alia Airport Road' , 'images/Country Club.jpg' , size[i], 'without DJ', 'without catring service');
-    new Branch('outdoor', 'dunes club','Close to Gamadan, Queen Alia Airport Road' ,'images/dunes club.jpg' , size[i], 'without DJ', 'without catring service');
-    new Branch('indoor', 'fairmont', ' 5th Circle, Amman','images/fairmont.jpg' , size[i], 'with DJ', 'with catring service');
-    new Branch('outdoor', 'fourseasons', ' Kindi St, Amman','images/fourseasons.jpg' , size[i], 'without DJ', 'without catring service');
-    new Branch('indoor', 'Numan', 'Wasfi At-Tall St., Amman','images/Numan.jpg' , size[i], 'without DJ', 'with catring service');
-    new Branch('indoor', 'rotana', 'Black Iris Street, Amman', 'images/rotana.jpg' , size[i], 'with DJ', 'without catring service');
-    new Branch('outdoor', 'W amman','Rafiq Al Hariri Ave, Amman','images/W amman.jpg' , size[i], 'with DJ', 'with catring service');
-    new Branch('indoor', 'white halls','Queen Alia Airport Road','images/white halls.jpg' , size[i], 'with DJ', 'without catring service');
-  }
 
-  let group;
-  if (this.setting === 'indoor' && this.size === size[2]) {
-    group = 'A';
-  }
-  if (this.setting === 'indoor' && this.size === (size[0] || size[1])) {
-    group = 'B';
-  }
-  if (this.setting === 'outdoor' && this.size === size[2]) {
-    group = 'C';
-  }
-  if (this.setting === 'outdoor' && this.size === (size[0] || size[1])) {
-    group = 'D';
-  }
+// Branch.prototype.render = function () {
+// // entering new locations
+//   for (let i = 0; i < size.length; i++) {
+//     new Branch('indoor', 'Adonees Hall', 'Az zarqa/Army st.', 'img/adonees.png', size[i], 'yes', 'yes', '30');
+//     new Branch('outdoor', 'Banafsaj Hall', 'Tlaa Al Ali - Amman', 'img/anafsaj.jpg', size[i], 'no', 'yes', '30');
+//     new Branch('outdoor', 'Country Club', 'Action Target, Queen Alia Airport Road', 'img/Country Club.jpg', size[i], 'no', 'no', '45');
+//     new Branch('outdoor', 'Dunes Club', 'Close to Gamadan, Queen Alia Airport Road', 'img/dunes club.jpg', size[i], 'no', 'no', '50');
+//     new Branch('indoor', 'Fairmont Hotel', ' 5th Circle, Amman', 'img/fairmont.jpg', size[i], 'yes', 'yes', '60');
+//     new Branch('outdoor', 'Four Seasons', ' Kindi St, Amman', 'img/fourseasons.jpg', size[i], 'no', 'no', '55');
+//     new Branch('indoor', 'Numan Hall', 'Wasfi At-Tall St., Amman', 'img/Numan.jpg', size[i], 'no', 'yes', '40');
+//     new Branch('indoor', 'Rotana Hotel', 'Black Iris Street, Amman', 'img/rotana.jpg', size[i], 'yes', 'no', '60');
+//     new Branch('outdoor', 'W Amman', 'Rafiq Al Hariri Ave, Amman', 'img/W amman.jpg', size[i], 'yes', 'yes', '50');
+//     new Branch('indoor', 'white Halls', 'Queen Alia Airport Road', 'img/white halls.jpg', size[i], 'yes', 'no', '55');
+//   }
 
-};
+//   // allow different size options for venues
+//   let group;
+//   if (this.setting === 'indoor' && this.size === size[2]) {
+//     group = 'A';
+//   }
+//   if (this.setting === 'indoor' && this.size === (size[0] || size[1])) {
+//     group = 'B';
+//   }
+//   if (this.setting === 'outdoor' && this.size === size[2]) {
+//     group = 'C';
+//   }
+//   if (this.setting === 'outdoor' && this.size === (size[0] || size[1])) {
+//     group = 'D';
+//   }
 
-function onlyOne(checkbox) {
-    var checkboxes = document.getElementsByName('check')
-  
-    checkboxes.forEach((item) => {
-        if (item !== checkbox) item.checked = false
-    })
+// };
+
+// function onlyOne(checkbox) {
+//   var checkboxes = document.getElementsByName('check');
+
+//   checkboxes.forEach((item) => {
+//     if (item !== checkbox) item.checked = false;
+//   });
+// }
+
+// var bName;
+// var lName;
+// var gName;
+// var glName;
+// var wDate;
+// var chosenSize;
+// var dj;
+// var catering;
+// var userSetting;
+
+// // event listener to determine form inputs
+// let form1 = document.getElementsById('user-form');
+// form1.addEventListener('submit', function (event) {
+//   event.preventDefault();
+
+//   bName = event.target.bName.value;
+//   lName = event.target.lName.value;
+//   gName = event.target.gName.value;
+//   glName = event.target.glName.value;
+//   wDate = event.target.date.value;
+//   var size = [document.getElementById('size0'), document.getElementById('size1'), document.getElementById('size2')];
+//   if (size[0].checked === true) {
+//     chosenSize = event.target.size0.value;
+//   } else if (size[1].checked === true) {
+//     chosenSize = event.target.size1.value;
+//   }
+//   else {
+//     chosenSize = event.target.size2.value;
+//   }
+//   var djIndex = [document.getElementById('dj1'), document.getElementById('dj2')];
+
+//   if (djIndex[0].checked) {
+//     dj = event.target.dj1.value;
+//   }
+
+//   if (djIndex[1].checked) {
+//     dj = event.target.dj2.value;
+//   }
+
+//   var caIndex = [document.getElementById('ca1'), document.getElementById('ca2')];
+
+//   if (caIndex[0].checked) {
+//     catering = event.target.ca1.value;
+//   }
+
+//   if (caIndex[1].checked) {
+//     catering = event.target.ca2.value;
+//   }
+
+//   console.log('in', bName, lName, gName, glName, wDate, chosenSize, dj, catering);
+// });
+// console.log('out', bName, lName, gName, glName, wDate, chosenSize, dj, catering);
+
+
+// test data
+var bName = 'Bride';
+var lName = 'B-Family';
+var gName = 'Groom';
+var glName = 'G-Family';
+var wDate = '27/9/2020';
+var chosenSize = 'medium';
+var dj = 'yes';
+var catering = 'yes';
+var userSetting = 'outdoor';
+
+
+// rendering suitable options after form is filler
+const resultSection = document.getElementById('results');
+function renderResults() {
+  for (let i = 0; i < options.length; i++) {
+    let articleEl = document.createElement('article');
+    resultSection.appendChild(articleEl);
+    let unorderedList = document.createElement('ul');
+    articleEl.appendChild(unorderedList);
+    let nameItem = document.createElement('li');
+    unorderedList.appendChild(nameItem);
+    nameItem.textContent = 'Name of location:' + options[i].name;
+    let addressItem = document.createElement('li');
+    unorderedList.appendChild(addressItem);
+    addressItem.textContent = 'Address: ' + options[i].location;
+    let settingItem = document.createElement('li');
+    unorderedList.appendChild(settingItem);
+    settingItem.textContent = 'Works best for: ' + options[i].setting + ' events.';
+    let sizeItem = document.createElement('li');
+    unorderedList.appendChild(sizeItem);
+    sizeItem.textContent = 'Fits a ' + options[i].size + ' group.';
+    let djItem = document.createElement('li');
+    unorderedList.appendChild(djItem);
+    djItem.textContent = 'DJ included in price: ' + options[i].dj;
+    let cateringItem = document.createElement('li');
+    unorderedList.appendChild(cateringItem);
+    cateringItem.textContent = 'Catering included in price: ' + options[i].catering;
+    let priceItem = document.createElement('li');
+    unorderedList.appendChild(priceItem);
+    priceItem.textContent = 'Starting price (per person): ' + options[i].price + ' JDs';
+    let imageEl = document.createElement('img');
+    articleEl.appendChild(imageEl);
+    imageEl.setAttribute('src', options[i].path);
+    imageEl.setAttribute('title', options[i].name);
+    let buttonEl = document.createElement('button');
+    articleEl.appendChild(buttonEl);
+    buttonEl.setAttribute('id', options[i].id);
+    buttonEl.setAttribute('class', 'button');
+    buttonEl.textContent = 'Select Venue';
+  }
 }
 
-form1.addEventListener('submit' , function (event){
-    event.preventDefault();
- 
-     var bName = event.target.bName.value;
-     var lName = event.target.lName.value;
-     var gName = event.target.gName.value;
-     var glName = event.target.glName.value;
-     var wDate = event.target.date.value; 
-     var size = [document.getElementById('size0'),document.getElementById('size1'),document.getElementById('size2')];
-     var choosenSize ;
-       if(size[0].checked == true )
-       {choosenSize= event.target.size0.value; }
-       else if(size[1].checked == true ){
-       choosenSize= event.target.size1.value;
-       }
-       else{
-        choosenSize= event.target.size2.value;
-       }
-     var djIndex = [document.getElementById("dj1"),document.getElementById("dj2")];
-
-     if (djIndex[0].checked){
-         var dj = event.target.dj1.value;
-     }
-     
-     if (djIndex[1].checked){
-        var dj = event.target.dj2.value;
+// function to filter branches based on user input and store into an array of objects
+let options = [];
+function filter(settingInput, sizeInput, djInput, cateringInput) {
+  for (let i = 0; i < branches.length; i++) {
+    if (branches[i].setting === settingInput) {
+      if (branches[i].size === sizeInput) {
+        if (branches[i].isDjIncluded === djInput) {
+          if (branches[i].catering === cateringInput) {
+            options.push(branches[i]);
+          }
+        }
+      }
     }
-    
-    var caIndex = [document.getElementById("ca1"),document.getElementById("ca2")];
+  }
+  renderResults();
+}
+filter(userSetting, chosenSize, dj, catering);
+console.log('result of filter ' + options);
 
-    if (caIndex[0].checked){
-        var catrering = event.target.ca1.value;
-    }
-    
-    if (caIndex[1].checked){
-       var catrering = event.target.ca2.value;
-   }
-   
-   console.log(bName,lName,gName,glName,wDate,choosenSize,dj,catrering);
- 
-
-});
 
---
+++ js/local.js
@@ -0,0 +1,106 @@
+'use strict';
+const inviteTemplates = [
+  ['img/invites/1.jpg', 'winter'],
+  ['img/invites/2.jpg', 'summer'],
+  ['img/invites/3.jpg', 'spring'],
+  ['img/invites/4.jpg', 'gold'],
+  ['img/invites/5.jpg', 'fall'],
+];
+
+let weddingVenue;
+let invitationTemp;
+
+// rendering invitation templates to select from
+const templateSection = document.getElementById('templates');
+function renderTemplates() {
+  const articleEl = document.createElement('article');
+  templateSection.appendChild(articleEl);
+  const headerEl = document.createElement('h4');
+  articleEl.appendChild(headerEl);
+  headerEl.textContent = 'Sample Invitation Generator';
+  const paragraphEl = document.createElement('p');
+  articleEl.appendChild(paragraphEl);
+  paragraphEl.textContent = 'Please select one of the below templates to generate your personalized sample invitation:';
+  for (let i = 0; i < inviteTemplates.length; i++) {
+    let linkEl = document.createElement('a');
+    articleEl.appendChild(linkEl);
+    linkEl.setAttribute('href', 'invite.html');
+    linkEl.setAttribute('target', '_blank');
+    let imageEl = document.createElement('img');
+    linkEl.appendChild(imageEl);
+    imageEl.setAttribute('src', inviteTemplates[i][0]);
+    imageEl.setAttribute('title', inviteTemplates[i][1]);
+    imageEl.setAttribute('id', inviteTemplates[i][1]);
+    imageEl.setAttribute('class', 'template-backgrounds');
+  }
+}
+// event listener to check for user location selection
+resultSection.addEventListener('click', selectLocationHandler);
+function selectLocationHandler(event) {
+  if (event.target.className === 'button') {
+    for (let i = 0; i < options.length; i++) {
+      if (event.target.id === options[i].id) {
+        weddingVenue = options[i].name;
+        console.log(weddingVenue);
+        renderTemplates();
+      }
+    }
+  }
+}
+console.log('check venue selected ' + weddingVenue);
+
+
+// store invitation information into an array of objects
+let invitations = [];
+function Invitation(bName, bFamily, gName, gFamily, wedDate, wedVenue, inviteTemp) {
+  this.bride = bName;
+  this.brideFamilyName = bFamily;
+  this.groom = gName;
+  this.groomFamilyName = gFamily;
+  this.weddingDay = wedDate;
+  this.chosenVenue = wedVenue;
+  this.invite = inviteTemp;
+  invitations.push(this);
+}
+console.log('check invitations ' + invitations);
+
+// event listener to check for user template selection
+templateSection.addEventListener('click', selectTemplateHandler);
+
+function selectTemplateHandler(event) {
+  if (event.target.id !== 'templates') {
+    for (let i = 0; i < inviteTemplates.length; i++) {
+      if (event.target.id === inviteTemplates[i][1]) {
+        invitationTemp = inviteTemplates[i][0];
+      }
+    }
+  }
+  new Invitation(bName, lName, gName, glName, wDate, weddingVenue, invitationTemp);
+  storeData();
+}
+
+// store invitations into local storage
+function storeData() {
+  const inviteDataString = JSON.stringify(invitations);
+  localStorage.setItem('invites', inviteDataString);
+}
+// get back data from local storage
+function retrieveData() {
+  let inviteDataString = localStorage.getItem('invites');
+
+  let inviteDataArray = JSON.parse(inviteDataString);
+  console.log(inviteDataArray);
+  // invitations.push(inviteDataArray);
+  for (let i = 0; i < inviteDataArray.length; i++) {
+    new Invitation(
+      inviteDataArray[i].bride,
+      inviteDataArray[i].brideFamilyName,
+      inviteDataArray[i].groom,
+      inviteDataArray[i].groomFamilyName,
+      inviteDataArray[i].weddingDay,
+      inviteDataArray[i].chosenVenue,
+      inviteDataArray[i].invite
+    );
+  }
+}
+retrieveData();
