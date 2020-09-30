f88ca355f7e4efd8df1558091cac8f9ff69d6e1d
sallytareq <sally.ammous@gmail.com>
2020-09-30 20:24:54 +0300

    factoring CSS

--- js/app.js
+++ js/app.js
@@ -19,23 +19,6 @@ function Branch(setting, name, location, image, size, isDjIncluded, catering, pr
   branches.push(this);
 }
 
-/* // image paths
-../img/adonees.png
-../img/banafsaj.jpg
-../img/country-club.jpg
-../img/dunes-club.jpg
-../img/dunes-club2.jpg
-../img/fairmont.jpg
-../img/fairmont2.jpg
-../img/fourseason.jpg
-../img/fourseason2.jpg
-../img/numan.jpg
-../img/rotana.jpg
-../img/rotana2.jpg
-../img/w-amman.jpg
-../img/white-halls.jpg
-*/
-
 for (let i = 0; i < size.length; i++) {
   new Branch('indoor', 'Adonees Hall', 'Az zarqa/Army st.', '../img/adonees.png', size[i], 'yes', 'no', '30');
   new Branch('indoor', 'Banafsaj Hall', 'Tlaa Al Ali - Amman', '../img/banafsaj.jpg', size[i], 'no', 'no', '30');
@@ -73,55 +56,38 @@ function formHandler(event) {
   wDate = event.target.date.value;
 
   chosenSize = document.getElementById('size').value;
-  console.log(chosenSize);
-
   const djIndex = [document.getElementById('dj1'), document.getElementById('dj2')];
-
-  console.log('dj index' + djIndex);
-
   if (djIndex[0].checked) {
     dj = event.target.dj1.value;
   }
-
   if (djIndex[1].checked) {
     dj = event.target.dj2.value;
   }
 
-  console.log('dj' + dj);
-
   const caIndex = [document.getElementById('ca1'), document.getElementById('ca2')];
-
-  console.log('caIndex' + caIndex);
-
   if (caIndex[0].checked) {
     catering = event.target.ca1.value;
   }
-
   if (caIndex[1].checked) {
     catering = event.target.ca2.value;
   }
-  console.log('catering' + catering);
 
   const doorIndex = [document.getElementById('in'), document.getElementById('out')];
-
-  console.log('doorIndex' + doorIndex);
-
   if (doorIndex[0].checked) {
     door = event.target.in.value;
   }
-
   if (doorIndex[1].checked) {
     door = event.target.out.value;
   }
-  console.log('door' + door);
-
-  console.log('in', bName, lName, gName, glName, wDate, chosenSize, dj, catering, door);
+  
+  // console.log('in', bName, lName, gName, glName, wDate, chosenSize, dj, catering, door);
   filter(door, chosenSize, dj, catering);
   document.getElementById('user-form').removeEventListener('submit', formHandler);
   document.getElementById('user-form').addEventListener('submit', function (event) {
     event.preventDefault();
   });
 }
+
 let options = [];
 // rendering suitable options after form is filler
 const resultSection = document.getElementById('results');
@@ -142,18 +108,6 @@ function renderResults() {
     const addressItem = document.createElement('li');
     unorderedList.appendChild(addressItem);
     addressItem.textContent = 'Address: ' + options[i].location;
-    // const settingItem = document.createElement('li');
-    // unorderedList.appendChild(settingItem);
-    // settingItem.textContent = 'Works best for: ' + options[i].setting + ' events.';
-    // const sizeItem = document.createElement('li');
-    // unorderedList.appendChild(sizeItem);
-    // sizeItem.textContent = 'Fits a ' + options[i].size + ' group.';
-    // const djItem = document.createElement('li');
-    // unorderedList.appendChild(djItem);
-    // djItem.textContent = 'DJ included in price: ' + options[i].dj;
-    // const cateringItem = document.createElement('li');
-    // unorderedList.appendChild(cateringItem);
-    // cateringItem.textContent = 'Catering included in price: ' + options[i].catering;
     const priceItem = document.createElement('li');
     unorderedList.appendChild(priceItem);
     priceItem.textContent = 'Starting price (per person): ' + options[i].price + ' JDs';
